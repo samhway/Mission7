0c9d1d3cd12e110289a1fd5fc34d7306eec53b40
samhway <83246475+samhway@users.noreply.github.com>
2022-02-16 13:44:47 -0700

    Add project files.

---
+++ Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Bookstore.Models;
+using Bookstore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IBookstoreRepository repo;
+        public HomeController(ILogger<HomeController> logger, IBookstoreRepository temp)
+        {
+            _logger = logger;
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BookViewModel
+            {
+                Books = repo.Books
+                .OrderBy(p => p.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    totalNumBooks = repo.Books.Count(),
+                    booksPerPage = pageSize,
+                    currentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bookstore/Models/ViewModels/PageInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int totalNumBooks { get; set; }
+        public int booksPerPage { get; set; }
+        public int currentPage { get; set; }
+        public int totalPages => (int) Math.Ceiling((double) totalNumBooks / booksPerPage);
+    }
+}
